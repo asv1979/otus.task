2659990182f165316818ab065a009398028d2999
Alexey Vasilyev <asv2108@gmail.com>
2021-08-30 15:36:52 +0300

    refactor code to several methods

--- src/MathStringChecker.php
+++ src/MathStringChecker.php
@@ -19,30 +19,56 @@ final class MathStringChecker
      */
     public function __invoke(string $mathString): array
     {
-        $result = [
+        $response = [
             'success' => true,
             'errMessage' => ''
         ];
+
+        if ($this->isWrongArguments($mathString)) {
+            $response['success'] = false;
+            $response['errMessage'] = 'Only math symbols!';
+
+            return $response;
+        }
+
+        if ($this->isWrongFormat($mathString)) {
+            $response['success'] = false;
+            $response['errMessage'] = 'The expression has wrong format!';
+        }
+
+        return $response;
+    }
+
+    /**
+     * Check right math format
+     *
+     * @param string $mathString
+     * @return bool
+     */
+    private function isWrongArguments(string $mathString): bool
+    {
         $wrongArgument = '#[a-z]#iu';
-        try {
-            if (preg_match($wrongArgument, $mathString)) {
-                throw new \InvalidArgumentException('Only math symbols');
-            }
-        } catch (\InvalidArgumentException $e) {
-            $result['success'] = false;
-            $result['errMessage'] = $e->getMessage();
-
-            return $result;
+        if (preg_match($wrongArgument, $mathString)) {
+            return true;
         }
 
-        //((44+6)/(2*6))
+        return false;
+    }
+
+    /**
+     * Check is right expression format
+     *
+     * @param string $mathString
+     * @return bool
+     */
+    private function isWrongFormat(string $mathString): bool
+    {
+        //example is - ((44+6)/(2*6))
         $strictMathStringPattern = '#^\(\(\d+(\+|-|/|\*)\d+\)(\+|-|/|\*)\(\d+(\+|-|/|\*)\d+\)\)$#';
-
         if (!preg_match($strictMathStringPattern, $mathString)) {
-            $result['success'] = false;
-            $result['errMessage'] = 'Error format';
+            return true;
         }
 
-        return $result;
+        return false;
     }
 }
